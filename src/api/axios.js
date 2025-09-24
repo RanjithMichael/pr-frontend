164b8171ea373d8403c1642521938894fc855204
RanjithMichael <branjithmichael@gmail.com>
2025-09-24 17:36:22 +0530

    updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 
-const BASE_URL = "https://pr-backend-1.onrender.com/api/auth"; 
-
+const BASE_URL = "https://pr-backend-2.onrender.com/api/auth"; 
 
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true, // needed for CORS with credentials
 });
-
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token")?.trim();
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
