f83a85ea6eb84caf397489fc5b6db35d32a7b808
RanjithMichael <branjithmichael@gmail.com>
2025-11-05 19:17:48 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Register from "./pages/Register";
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Dashboard() {
   return (
     <div className="card p-4">
